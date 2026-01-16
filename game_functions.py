a9d768ae2f04ac85e87b9b6e51cd4cdde5232bba
HowardAlix <hfga201@exeter.ac.uk>
2026-01-16 12:25:20 +0000

    fixed

--- game_functions.py
+++ game_functions.py
@@ -7,6 +7,12 @@ def pick_value(poss_values):
 
 # function to be used in game_2: Higher or Lower
 def check_higher_lower(current_val, next_val, user_input):
+    if user_input =="h":
+        return next_val > current_val
+    elif user_input =="l":
+        return next_val< current_val
+    else:
+        return False
     pass
 
 # function to be used in game_3: Hangman
